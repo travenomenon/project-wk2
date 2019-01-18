96aa4ffe87ca2ba14e44aeed49e91bf8215d1240
Student <example@example.com>
2019-01-18 10:09:15 -0800

    progress on making form function

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+  var question1Input = $("input#question1").val()
+  var question2Input = $("input#question2").val()
+  var question3Input = $("input#question3").val()
+  var question4Input = $("input#question4").val()
+  var question5Input = $("input#question5").val()
+
+
+
+
+
+
+
+  }
+}
