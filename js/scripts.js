3bee93006012348ec0c386348d72f2f6e246aba6
Student <example@example.com>
2019-01-18 11:02:22 -0800

    make form results work correctly

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,17 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-  var question1Input = $("input#question1").val()
-  var question2Input = $("input#question2").val()
-  var question3Input = $("input#question3").val()
-  var question4Input = $("input#question4").val()
-  var question5Input = $("input#question5").val()
+
+    var question1Input = $(".question1").val()
+    var question2Input = $(".question2").val()
+    var question3Input = $(".question3").val()
+    var question4Input = $(".question4").val()
+    var question5Input = $(".question5").val()
+
+    if (question1Input === question3Input) {
+      $("#ruby").show();
+    }
 
 
-
-
-
-
-
-  }
-}
+    event.preventDefault();
+  });
+});
