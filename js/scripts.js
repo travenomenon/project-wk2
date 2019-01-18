f4750be4b8b5e22fa929ce19b872afbac9570084
Student <example@example.com>
2019-01-18 12:35:20 -0800

    add images to results

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,18 @@ $(document).ready(function() {
     var question4Input = $(".question4").val()
     var question5Input = $(".question5").val()
 
-    if (question1Input === question3Input) {
+    if (question1Input === question3Input || question2Input != question4Input || question2Input != question5Input) {
       $("#ruby").show();
     }
+    if (question2Input === question4Input || question1Input != question3Input || question2Input != question5Input) {
+      $("#php").show();
+    }
+
+
+
+
+
+
 
 
     event.preventDefault();
