077d09e33103d7bba6a647f068fe24d992f21e2a
travenomenon <beardohweirdoh@gmail.com>
2019-01-20 15:37:42 -0800

    fix if else logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,34 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
 
-    var question1Input = $(".question1").val()
-    var question2Input = $(".question2").val()
-    var question3Input = $(".question3").val()
-    var question4Input = $(".question4").val()
-    var question5Input = $(".question5").val()
+    $("#ruby, #php, #cSharp").hide();
+    //reset track after new form submission
 
-    if (question1Input === question3Input || question2Input != question4Input || question2Input != question5Input) {
+    var question1Input = $("#question1").val();
+    var question2Input = $("#question2").val();
+    var question3Input = $("#question3").val();
+    var question4Input = $("#question4").val();
+    var question5Input = $("#question5").val();
+
+    if (question1Input === question3Input
+      && question2Input != question4Input
+      && question2Input != question5Input
+      && question1Input === 'yes') {
       $("#ruby").show();
-    }
-    if (question2Input === question4Input || question1Input != question3Input || question2Input != question5Input) {
+    } else if (question2Input === question4Input
+      && question1Input != question3Input
+      && question2Input != question5Input
+      && question2Input === 'yes') {
       $("#php").show();
+    } else if (question2Input === question5Input
+      && question1Input != question3Input
+      && question2Input != question4Input
+      && question2Input === 'yes') {
+      $("#cSharp").show();
+    } else {
+      $("#php").show();
+      //default choice if above logic does not match
     }
-
-
-
-
-
-
-
 
     event.preventDefault();
   });
